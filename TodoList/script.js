30b3f22ff65a6b2377f55f8a759b55e50d8f36ef
Frederick-js <faivergarzon@gmail.com>
2024-02-02 13:48:21 -0500

    todo list terminado

--- TodoList/script.js
+++ TodoList/script.js
@@ -2,18 +2,24 @@ const fecha = document.querySelector("#fecha");
 const lista = document.querySelector("#lista");
 const input = document.querySelector("#input");
 const botonEnter = document.querySelector("#enter");
-const check = 'fa-check-circle';
-const uncheck = 'fa-circle';
-const lineThrough = 'line-through';
-let id = 0;
+const check = "fa-check-circle";
+const uncheck = "fa-circle";
+const lineThrough = "line-through";
+let id
+let LIST
+
+
+// Creacion de funcion fecha
+const FECHA = new Date();
+fecha.innerHTML= FECHA.toLocaleDateString('es-MX', {weekday:'long', month: 'short', day: 'numeric'});
+
 
 // Funcion agregar tarea
-function agregarTarea(tarea,id,realizado,eliminado) {
-
+function agregarTarea(tarea, id, realizado, eliminado) {
   if (eliminado) {return};
 
   const REALIZADO = realizado ? check : uncheck;
-  const LINE = realizado ? lineThrough: "";
+  const LINE = realizado ? lineThrough : "";
   const elemento = `<li id="elemento">
   <i class="far ${REALIZADO}" data="realizado" id='${id}'></i>
   <p class="text ${tarea}">${tarea}</p>
@@ -26,47 +32,70 @@ function agregarTarea(tarea,id,realizado,eliminado) {
 function tareaRealizada(element) {
   element.classList.toggle(check);
   element.classList.toggle(uncheck);
-  element.parentNode.querySelector('.text').classList.toggle(lineThrough);
+  element.parentNode.querySelector(".text").classList.toggle(lineThrough);
+  LIST[element.id].realizado = LIST[element.id].realizado ? false : true;
+
 }
 
 //  Funcion tarea Eliminada
 function tareaEliminar(element) {
   element.parentNode.parentNode.removeChild(element.parentNode);
+  LIST[element.id].eliminado = true;
 }
-
-
-
-
-
 
 botonEnter.addEventListener("click", () => {
   const tarea = input.value;
   if (tarea) {
-    agregarTarea(tarea,id,false,false);
-  }
+    agregarTarea(tarea, id, false, false);
+    LIST.push({
+      nombre: tarea,
+      id:id,
+      realizado:false,
+      eliminado:false
+    })
+  localStorage.setItem('TODO', JSON.stringify(LIST));
+  id++;
   input.value = "";
-  id++     
+  }
 });
 
 document.addEventListener("keyup", function (event) {
   if (event.key == "Enter") {
     const tarea = input.value;
     if (tarea) {
-      agregarTarea(tarea,id,false,false);
+      agregarTarea(tarea, id, false, false);
     }
+    localStorage.setItem('TODO', JSON.stringify(LIST));
     input.value = "";
-    id++
+    id++;
   }
 });
 
-lista.addEventListener('click', function(event) {
+lista.addEventListener("click", function (event) {
   const element = event.target;
   const elementData = element.attributes.data.value;
-  if(elementData === 'realizado'){
+  if (elementData === "realizado") {
     tareaRealizada(element);
-  }
-  else if (elementData === 'eliminado') {
+  } else if (elementData === "eliminado") {
     tareaEliminar(element);
   }
+  localStorage.setItem('TODO', JSON.stringify(LIST));
+});
 
-})
+
+// local storage set item
+let data = localStorage.getItem('TODO');
+if(data){
+  LIST=JSON.parse(data);
+  id = LIST.length;
+  cargarLista(LIST);
+}else {
+  LIST = [];
+  id = 0;
+}
+
+function cargarLista(DATA){
+  DATA.forEach(function(i) {
+    agregarTarea(i.nombre,i.id,i.realizado,i.eliminado)
+  });
+}
