e1dfa957de403c79288af306321b44818f9c2e76
Frederick-js <faivergarzon@gmail.com>
2024-02-05 00:06:58 -0500

    avanzando con proyecto crud

---
+++ crud/app.js
@@ -0,0 +1,11 @@
+const newMemberAddBtn = document.querySelector('.addMemberBtn');
+const darkBg = document.querySelector('.dark_bg');
+const popupForm = document.querySelector('.popup');
+const crossBtn = document.querySelector('.closeBtn');
+
+newMemberAddBtn.addEventListener('click', ()=>{
+    console.log('hola');
+})
+
+
+console.log(newMemberAddBtn);
