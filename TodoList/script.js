a7a9c15cd0ecd078383b76a4bc00dae8f454d4ff
Frederick-js <faivergarzon@gmail.com>
2024-02-04 21:39:21 -0500

    restaurando

--- TodoList/script.js
+++ TodoList/script.js
@@ -60,7 +60,8 @@ botonEnter.addEventListener("click", () => {
     id++;
     input.value = "";
   }else{
-    alert('Olvidaste agregar la tarea');
+    const modalId = this.dataset.open;
+    document.getElementById(modalId).classList.add(isVisible);
   }
 });
 
@@ -105,3 +106,34 @@ function cargarLista(DATA) {
     agregarTarea(i.nombre, i.id, i.realizado, i.eliminado);
   });
 }
+
+
+
+
+
+
+for (const el of openEls) {
+  el.addEventListener("click", function() {
+    const modalId = this.dataset.open;
+    document.getElementById(modalId).classList.add(isVisible);
+  });
+}
+
+for (const el of closeEls) {
+  el.addEventListener("click", function() {
+    this.parentElement.parentElement.parentElement.classList.remove(isVisible);
+  });
+}
+
+document.addEventListener("click", e => {
+  if (e.target == document.querySelector(".modal.is-visible")) {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
+  }
+});
+
+document.addEventListener("keyup", e => {
+  // if we press the ESC
+  if (e.key == "Escape" && document.querySelector(".modal.is-visible")) {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
+  }
+});
