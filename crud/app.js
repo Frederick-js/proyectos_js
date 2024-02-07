a0e54110983b25e084f75336f30fc86f70546c93
Frederick-js <faivergarzon@gmail.com>
2024-02-07 14:38:23 -0500

    actualizando formulario

--- crud/app.js
+++ crud/app.js
@@ -6,7 +6,23 @@ const newMemberAddBtn = document.querySelector(".addMemberBtn"),
   modalTitle = document.querySelector(".modalTitle"),
   popupFooter = document.querySelector(".popupFooter"),
   imgInput = document.querySelector(".img"),
-  form = document.querySelector("form")
+  form = document.querySelector("form"),
+  uploadimg = document.querySelector("#uploadimg"),
+  fName = document.getElementById("fName"),
+  lName = document.getElementById("lName"),
+  age = document.getElementById("age"),
+  city = document.getElementById("city"),
+  position = document.getElementById("position"),
+  salary = document.getElementById("salary"),
+  sDate = document.getElementById("sDate"),
+  email = document.getElementById("email"),
+  phone = document.getElementById("phone")
+
+
+let originalData = localStorage.getItem('userProfile') ? JSON.parse(localStorage.getItem('userProfile')) : [];
+let getData = [...originalData];
+
+let isEdit = false, edit
 
 newMemberAddBtn.addEventListener("click", () => {
   submitBtn.innerHTML = "Submit";
@@ -24,3 +40,47 @@ crossBtn.addEventListener('click', ()=>{
   popupForm.classList.remove("active");
   form.reset();
 })
+
+uploadimg.onchange = function (){
+    if (uploadimg.files[0].size < 1000000) {  // 1MB = 1000000
+        var fileReader = new FileReader();
+        
+        fileReader.onload = function(e){
+            var imgUrl = e.target.result;
+            imgInput.src = imgUrl;
+        }
+
+        fileReader.readAsDataURL(uploadimg.files[0]);
+    }
+    else{
+        alert("Thist file is too large!")
+    }
+}
+
+form.addEventListener('submit', (e)=> {
+    e.preventDefault();
+
+    const information = {
+        id: Date.now();
+        picture: imgInput.src == undefined ? "./img/pic1.png" :imgInput.src,
+        fName: fName.value,
+        lName: lName.value, 
+        ageVal: ageVal.value, 
+        cityVa: cityVa.value, 
+        positionVa: positionVa.value, 
+        salaryVa: salaryVa.value, 
+        sDateVa: sDateVa.value, 
+        phoneVAl: phoneVAl.value
+       
+    }
+})
+
+if (!isEdit) {
+    originalData.unshift(information);
+}
+else{
+    originalData[editId] = information;
+}
+
+getData = [...originalData];
+localStorage.setItem('userProfile', JSON.stringify(originalData));
