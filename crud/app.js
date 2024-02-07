84f151146323eb1ba2f89ce4faf7aa262f5f4616
Frederick-js <faivergarzon@gmail.com>
2024-02-07 12:06:11 -0500

    update

--- crud/app.js
+++ crud/app.js
@@ -1,11 +1,9 @@
-const newMemberAddBtn = document.querySelector('.addMemberBtn');
-const darkBg = document.querySelector('.dark_bg');
-const popupForm = document.querySelector('.popup');
-const crossBtn = document.querySelector('.closeBtn');
+const newMemberAddBtn = document.querySelector(".addMemberBtn"),
+  darkBg = document.querySelector(".dark_bg"),
+  popupForm = document.querySelector(".popup"),
+  crossBtn = document.querySelector(".closeBtn");
 
-newMemberAddBtn.addEventListener('click', ()=>{
-    darkBg.classList.add('active')
-    popupForm.classList.add('active')
-})
-
-
+newMemberAddBtn.addEventListener("click", () => {
+  darkBg.classList.add("active");
+  popupForm.classList.add("active");
+});
